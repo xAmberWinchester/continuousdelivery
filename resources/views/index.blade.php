2d98c4716cd0e2315765a0dc94b2d7680d58f438
Amber <amber@degronckel.com>
2020-08-31 23:14:07 +0200

    Lorum ipsum

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,10 +17,8 @@
 <body>
 <div class="inhoud">
     <div class="Kenmerken">
-        <h3>Love it</h3>
+        <h3>Lorum ipsum</h3>
         <ul>
-            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
             <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
             <li>Aenean ac lectus in ex hendrerit posuere et eget lectus</li>
             <li>Vestibulum ornare dapibus ex, vel lobortis orci commodo at</li>
